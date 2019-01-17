c8afc8f4d055164098c58262d08b0f7771ab161f
R. Filbin <robert.filbin@dyso.co>
2019-01-17 00:26:58 -0500

    Updates

---
+++ precache-manifest.a07a150bf99995e157c7af269d6d39f6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "16d37a92511b7bab459a",
+    "url": "/react-js-dashboard-example/static/css/main.a50f9e18.chunk.css"
+  },
+  {
+    "revision": "16d37a92511b7bab459a",
+    "url": "/react-js-dashboard-example/static/js/main.16d37a92.chunk.js"
+  },
+  {
+    "revision": "4f95772ff8a79abc0528",
+    "url": "/react-js-dashboard-example/static/js/1.4f95772f.chunk.js"
+  },
+  {
+    "revision": "aad2501c000092153f25",
+    "url": "/react-js-dashboard-example/static/js/runtime~main.aad2501c.js"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/react-js-dashboard-example/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "9fcf69e5359987c9b1545aae1c2c5faa",
+    "url": "/react-js-dashboard-example/static/media/emilyz.9fcf69e5.jpg"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/react-js-dashboard-example/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/react-js-dashboard-example/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/react-js-dashboard-example/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/react-js-dashboard-example/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "5d19ad8e95d829d381a4ed512080d7b3",
+    "url": "/react-js-dashboard-example/index.html"
+  }
+];
